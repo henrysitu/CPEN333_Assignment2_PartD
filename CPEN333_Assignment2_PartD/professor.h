ffbfd6151c8e7588d9fbe54fd8187ef161d7a690
Henry <imhenrysitu@gmail.com>
2020-12-03 23:34:30 -0800

    init professor commit

---
+++ CPEN333_Assignment2_PartD/professor.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef __professor__
+#define __professor__
+
+class professor {
+private:
+	string course;
+	int numStudents;
+	float grades[];
+public:
+	void submitGrades() {
+
+	}
+	void modifyGrades() {
+
+	}
+};
+#endif // !__professor__
