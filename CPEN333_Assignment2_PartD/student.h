b12e510991d1150c5df0fa802130472d6701991f
Henry <imhenrysitu@gmail.com>
2020-12-03 22:30:08 -0800

    init push

---
+++ CPEN333_Assignment2_PartD/student.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+#ifndef __student__
+#define __student__
+
+using namespace std;
+
+struct preferenceForm {
+	string firstChoice;
+	string secondChoice;
+	string thirdChoice;
+	string personalStatement;
+};
+
+class student {
+private:
+	string name;
+	int studentNumber;
+	string SSCuserID;
+	string SSCPassword;
+	preferenceForm selectionForm;
+	string enroledCourses[];
+public:
+	void submitSelectionForm(preferenceForm form) {
+
+	}
+	void payFees(float amount) {
+
+	}
+	void cheat() {
+
+	}
+	void registerForCourse(string course) {
+
+	}
+};
+
+#endif // !__student__
