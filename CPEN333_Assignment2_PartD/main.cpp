a3fa865e48931aff08a1b9a7ef1c19788f393a7a
chenyiz_cpen <chenyizheng97@gmail.com>
2020-12-04 22:26:16 -0800

    initialize variables

--- CPEN333_Assignment2_PartD/main.cpp
+++ CPEN333_Assignment2_PartD/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+
 //#include "academicRecords.h"
 //#include "bank.h"
 //#include "president.h"
@@ -9,6 +10,7 @@
 //#include "selectionForm.h"
 //#include "SSCWebsite.h"
 //#include "student.h"
+
 #include "E:\UBC\2020-2021\CPEN 333\Assignments\Assignment 2\Assignment2PartD\CPEN333_Assignment2_PartD\academicRecords.h"
 #include "E:\UBC\2020-2021\CPEN 333\Assignments\Assignment 2\Assignment2PartD\CPEN333_Assignment2_PartD\bank.h"
 #include "E:\UBC\2020-2021\CPEN 333\Assignments\Assignment 2\Assignment2PartD\CPEN333_Assignment2_PartD\president.h"
@@ -19,20 +21,77 @@
 #include "E:\UBC\2020-2021\CPEN 333\Assignments\Assignment 2\Assignment2PartD\CPEN333_Assignment2_PartD\student.h"
 
 int main(void) {
-	academicRecords academicRecords_Student1;
-	bank BankAccount_Student1;
-	president thePresident;
-	professor Professor1;
-	selectionAlgorithm engSelectionAlgorithm;
-	selectionForm SelectionForm_Student1;
-	SSCWebsite sscWebsite;
-	student Student1;
+
+	string aCourse = "MECH420";
+	int totalCredits = 3;
+	float aStudentGrade = 90.0;
+
+	// student
+	bank* bankAccount;
+	SSCWebsite* SSCAccount;
+	selectionForm* preferenceForm;
+	int studentNum = 12345678;
+	string studentName = "AStudent";
+	secYearSelectionForm selectionForm;
+	selectionForm.firstChoice = "MECH";
+	selectionForm.secondChoice = "CPEN";
+	selectionForm.thirdChoice = "ELEC";
+	selectionForm.personalStatement = "abc";
+
+	string SSCuserID;
+	string SSCPassword;
+	vector<string> enrolledCourses;
+
+
+	// academic records
+	float gpa = 3.8;
+	transcript transcripts;
+	string program = "MECH";
+	cheatingRecords studentCheatingRecords;
+
+	// bank 
+	float bankBalance = 10000;
+	SSCWebsite* ssc;
+
+	// president
+	///ssc
+	/// 
+	// professor
+	int numStudents = 1;
+	studentGrades student1Grade;
+	student1Grade.studentNumber = studentNum; // needs to be the same as student
+	student1Grade.grade = aStudentGrade;
+	string courseToSubmitGrade = "MECH420";
+	///ssc
+
+	// selection algorithm
+	academicRecords* records;
+	selectionForm* filledForm;
+	///float gpa;
+	
+	// selection form
+	///no private var
+	
+	// SSC Website
+	studentInfo info;
+	info.studentNumber = studentNum;
+	info.name = studentName;
+	info.courses.push_back(aCourse);
+	info.grades.push_back(aStudentGrade);
+	int credits = totalCredits;
+	///string program
+	bool suspended = false;
+	float fees = 3000;
+	enrolledCourses listOfCourses;
+	listOfCourses.enrolledCourses.push_back(aCourse);
+	listOfCourses.grades.push_back(aStudentGrade);
+	listOfCourses.credits = totalCredits;
 
 	string courseToRegister = "CPEN333";
 	int userInput;
 	int amount = 5000;
 
-	string courseToSubmitGrade = "MECH420";
+	
 	while (1)
 	{
 		cout << "If the user is a student: \n";
